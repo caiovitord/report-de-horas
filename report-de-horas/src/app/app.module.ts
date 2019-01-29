4fc5fd8b69d6af2a3320207b633b944b9f1a4c89
CaioVitor <caio.gobira@integradorsolar.com>
2019-01-29 13:50:28 -0200

    Revert "Componente de teste, instalado o AdminLTE"
    This reverts commit 327004dc0ca4a6f6cb46825c8af44636625767ac.

--- report-de-horas/src/app/app.module.ts
+++ report-de-horas/src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SaveComponent } from './testes/save/save.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SaveComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule 
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
